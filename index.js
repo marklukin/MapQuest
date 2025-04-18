abb85d116fd0057297edd603fac4d3cf71400473
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-04-18 13:09:54 +0300

    feat: implement basic backend server

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log('Hello World');
+const http = require('node:http');
+const fs = require('node:fs');
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const index = (req, resp) => {
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    };
+    resp.writeHead(200, { 'Content-Type': 'text/html' });
+    resp.write(data);
+    resp.end();
+  });
+};
+
+const server = http.createServer((req, resp) => {
+  if (req.url === '/') index(req, resp);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server has started at http://${hostname}:${port}`);
+});
+
+server.on('error', (err) => {
+  if (err.code === 'EACCES') {
+    console.log(`No access to port: ${port}`);
+  }
+});
