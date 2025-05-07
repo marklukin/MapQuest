777bdd8a1df33a6710b853980b2676423e7ef440
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-05-07 20:28:01 +0300

    ref: delete unnecessary tables

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,28 +8,12 @@ const createDatabase = () => {
     CREATE TABLE IF NOT EXISTS Players(
       Playerid INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT NOT NULL,
-      hashed_password TEXT NOT NULL,
+      password_hash TEXT NOT NULL,
+      password_salt TEXT NOT NULL,
       world_score INTEGER DEFAULT 0,
       europe_score INTEGER DEFAULT 0,
       asia_score INTEGER DEFAULT 0,
       usa_score INTEGER DEFAULT 0
-    )
-  `);
-
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS Admins(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT NOT NULL,
-      hashed_password TEXT NOT NULL,
-    )
-  `);
-
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS JWTokens(
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      creator TEXT NOT NULL,
-      token TEXT NOT NULL,
-      expire TEXT NOT NULL,
     )
   `);
 
