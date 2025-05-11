257c1cd401537b9b7a6e767aa8c53eca93d48b62
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-05-11 13:12:11 +0300

    feat: change field name Playerid to player_id

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,7 @@ const db = new DatabaseSync('./database.db');
 const createDatabase = () => {
   db.exec(`
     CREATE TABLE IF NOT EXISTS Players(
-      Playerid INTEGER PRIMARY KEY AUTOINCREMENT,
+      player_id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT NOT NULL,
       password_hash TEXT NOT NULL,
       password_salt TEXT NOT NULL,
@@ -18,7 +18,7 @@ const createDatabase = () => {
       usa_score INTEGER DEFAULT 0
     )
   `);
-
+  
   return db;
 };
 
