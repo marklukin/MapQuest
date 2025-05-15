6fbceb731cecb5423ae98301dc49a609e77c2935
Mark Lukin <mark.m.lukin@gmail.com>
2025-05-15 21:46:37 +0200

    adding a queue

---
+++ public/priorityQueue.js
@@ -0,0 +1,20 @@
+export class BiDirectionalPriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return null;
+    return this.items.splice(0, 1)[0];
+  }
+
+
+isEmpty() {
+  return this.items.length === 0;
+}
+
+}
