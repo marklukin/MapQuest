b058a02022c8e878624a63ef486bbb7d08af6287
fakeowl1 <ukolov.roman@lll.kpi.ua>
2025-05-24 11:23:51 +0300

    feat: update expiry time, if item is used

--- public/utils/memoize.js
+++ public/utils/memoize.js
@@ -1,14 +1,14 @@
 export function memoize(func, ttlMinutes = 60, maxSize = 1000) {
   const cache = new Map();
-
+  
   return async function(...args) {
     if (cache.size > maxSize) cache.clear();
 
-    const key = JSON.stringify(args);
+    const key = JSON.stringify(args)
     const now = Date.now();
     const item = cache.get(key);
     
-    if (item) { // Update expire date, if item used
+    if (item) { // Update expiry, if item is used
       item.expiry = now + (ttlMinutes/2 * 60 * 1000);
       return item.value;
     }
